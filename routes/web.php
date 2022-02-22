fbc7edae9b57135700efb16add2d306ad9089d7f
Everton Silva <57024315+evertonos2312@users.noreply.github.com>
2022-02-22 16:12:59 -0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'PrincipalController@principal');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 Route::get('/contato', 'ContatoController@contato');
+Route::get('/login', function () {return 'Login';});
+
+Route::prefix('/app')->group(function (){
+    Route::get('/clientes', function () {return 'Clientes';});
+    Route::get('/fornecedores', function () {return 'Fornecedores';});
+    Route::get('/produtos', function () {return 'Produtos';});
+});
